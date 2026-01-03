44780bebe53914ef8c4cca4d7210415b1ce43768
gongzhongqiang <gongzhongqiang@apache.org>
2026-01-03 10:42:27 +0800

    feat: enhance scrollbar styles for improved appearance in light and dark modes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/globals.css";
 import { initializeTheme } from "./lib/utils";
 
 // Initialize theme before rendering
